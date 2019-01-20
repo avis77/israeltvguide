b3e42dcc4b1b5585293c658a9170d520cbecd504
avis77 <aviram.sht@gmail.com>
2019-01-20 14:49:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  console.log('got ${req}')
+  console.log(`got ${req} ${req}`)
   var request = require('sync-request');
   var res = request('GET','https://www.mako.co.il/AjaxPage',{
       qs:{jspName:'EPGResponse.jsp'}
   });
   const data = JSON.parse(res.body.toString('utf-8'));
-console.log('got ${data}')
+console.log(`got ${data}`)
   res.send('Hello World!')
   
 })

978497b623ccfafecfb78c03ebe517b9551642cf
avis77 <aviram.sht@gmail.com>
2019-01-21 09:25:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,68 @@
-const express = require('express')
-const app = express()
 const port = process.env.PORT || 5000
+const express = require('express')
+const bodyParser = require('body-parser')
+const { dialogflow } = require('actions-on-google')
 
-app.get('/', (req, res) => {
-  console.log('get')
-  console.log(req.query)
-  console.log(req.params)
-  var request = require('sync-request');
-  var result = request('GET','https://www.mako.co.il/AjaxPage',{
-      qs:{jspName:'EPGResponse.jsp'}
-  });
-  const data = JSON.parse(result.body.toString('utf-8'));
-console.log(`got ${data.programs[1].ProgramName}`)
-  res.send('Hello World!')
-  
+const app = dialogflow()
+express().use(bodyParser.json(), app).listen(port)
+
+app.intent('get', conv => {
+  conv.ask(`The last thing I told you was ${conv.randomize.last}`)
+  conv.ask(new Randomization(
+    'How are you?',
+    'Are you having a good day?',
+  ))
 })
 
-app.post('/', (req, res) => {
-  console.log('post')
-  console.log(req.query)
-  console.log(req.params)
+// app.get('/', (req, res) => {
+//   console.log('get')
+//   console.log(req.query)
+//   console.log(req.params)
+//   var request = require('sync-request');
+//   var result = request('GET','https://www.mako.co.il/AjaxPage',{
+//       qs:{jspName:'EPGResponse.jsp'}
+//   });
+//   const data = JSON.parse(result.body.toString('utf-8'));
+// console.log(`got ${data.programs[1].ProgramName}`)
+//   res.send('Hello World!')
   
-  // Import the appropriate class
-  const { WebhookClient } = require('dialogflow-fulfillment');
+// })
 
-  //Create an instance
-  const agent = new WebhookClient({request: req, response: res});
+// app.post('/', (req, res) => {
+//   console.log('post')
+//   console.log(req.query)
+//   console.log(req.params)
+  
+//   // Import the appropriate class
+//   const { WebhookClient } = require('dialogflow-fulfillment');
 
-  function welcome(agent) {
-    agent.add(`hi this is israel tv guide`);
-  }
+//   //Create an instance
+//   const agent = new WebhookClient({request: req, response: res});
+
+//   function welcome(agent) {
+//     agent.add(`hi this is israel tv guide`);
+//   }
  
-  function fallback(agent) {
-    agent.add(`like a cow, what do you want?`);
-  }
+//   function fallback(agent) {
+//     agent.add(`like a cow, what do you want?`);
+//   }
 
-  function yourFunctionHandler(agent) {
-    var request = require('sync-request');
-    var result = request('GET','https://www.mako.co.il/AjaxPage',{
-        qs:{jspName:'EPGResponse.jsp'}
-    });
-    const data = JSON.parse(result.body.toString('utf-8'));
-    console.log(`got ${data.programs[1].ProgramName}`)
-    agent.add('${data.programs[1].ProgramName}')
-  }
+//   function yourFunctionHandler(agent) {
+//     var request = require('sync-request');
+//     var result = request('GET','https://www.mako.co.il/AjaxPage',{
+//         qs:{jspName:'EPGResponse.jsp'}
+//     });
+//     const data = JSON.parse(result.body.toString('utf-8'));
+//     console.log(`got ${data.programs[1].ProgramName}`)
+//     agent.add('${data.programs[1].ProgramName}')
+//   }
   
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('get', yourFunctionHandler);
+//   let intentMap = new Map();
+//   intentMap.set('Default Welcome Intent', welcome);
+//   intentMap.set('Default Fallback Intent', fallback);
+//   intentMap.set('get', yourFunctionHandler);
 
-  agent.handleRequest(intentMap);
-})
+//   agent.handleRequest(intentMap);
+// })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))

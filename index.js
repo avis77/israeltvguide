9baba3d52117b3f2dbd4a9b80db2cf4f1863fb9d
avis77 <aviram.sht@gmail.com>
2019-01-21 16:56:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const curl = new (require( 'curl-request' ))();
 curl.setHeaders([
     'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
 ])
-.get('http://reshet.tv/general/tv-guide/')
+.get('https://reshet.tv/general/tv-guide/')
 .then(({statusCode, body, headers}) => {
     console.log(statusCode, body, headers)
 })
